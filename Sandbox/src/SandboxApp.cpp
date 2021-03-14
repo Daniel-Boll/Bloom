aacec5f53e500389e8fff2f40d8c443f04051565
Daniel Boll <danielboll.academico@gmail.com>
2021-03-14 18:06:25 -0300

    Adding Sandbox
    Inherit Application from Bloom engine

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,16 @@
+#include <Bloom.h>
+
+/*
+	Create a Sandbox class that inherit
+	everything from the Bloom::Application
+*/
+class Sandbox : public Bloom::Application {
+	public:
+		Sandbox() {}
+
+		~Sandbox() {}
+};
+
+Bloom::Application* Bloom::CreateApplication() {
+	return new Sandbox();
+}
